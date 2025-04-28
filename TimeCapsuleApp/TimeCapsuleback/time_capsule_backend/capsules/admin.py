3293df405281808269b840b317dae05afb1e52a0
Dariya <156985567+Thinkabouthev@users.noreply.github.com>
2025-04-29 00:06:49 +0500

    Delete TimeCapsuleApp/TimeCapsuleback/time_capsule_backend directory

--- TimeCapsuleApp/TimeCapsuleback/time_capsule_backend/capsules/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Capsule
-admin.site.register(Capsule)
-# Register your models here.
--- TimeCapsuleApp/TimeCapsuleback/time_capsule_backend/capsules/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import CapsuleViewSet
-
-router = DefaultRouter()
-router.register('capsules', CapsuleViewSet)
-
-urlpatterns = router.urls
